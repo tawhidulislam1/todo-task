ad34c9e7ed4a5ccacd66b4c2200a2904f73b4f0d
unknown <designwithtawhid@gmail.com>
2024-11-04 20:27:37 +0600

    hero updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,18 +4,18 @@ import image from '../../assets/banner.jpg'
 const Hero = () => {
     return (
         <div>
-            <div className="hero min-h-screen text-white relative">
+            <div className="hero min-h-[80vh] text-white relative">
                 <div className="hero-content text-center">
                     <div className=" max-w-screen-lg">
                         <h1 className="text-5xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
                         <p className="py-6">
-                        Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
+                            Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
                         </p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
-                <div className="absolute">
-                    {/* <img src={image} alt="" /> */}
+                <div className="absolute -bottom-1/3 ">
+                    <img src={image} alt="" className='w-[910px] border-spacing-1.5 h-[450px] border rounded-md' />
                 </div>
             </div>
 
